7073d599b792cc5dd4d63fc8b19186ba755b3f29
doublepiece1 <doublepiece1@github.com>
2026-07-24 09:54:02 +0900

    今日４０°C超えるらしい

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 3;
+    [SerializeField] float rotateSpeed = 20;
+
+    Rigidbody rb;
+
+    public Collider playerCollider { get; set; }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = playerCollider.bounds.center - rb.position;
+
+        bool isSeenPlayer = true;
+        if(Physics.Raycast(rb.position, direction.normalized,
+            out var hitInfo))
+        {
+            if(hitInfo.collider != playerCollider)
+            {
+                // プレイヤー以外の障害物に当たった場合は見えない1
+                isSeenPlayer = false;
+            }
+        }
+
+        if (isSeenPlayer)
+        {
+            var subVec = playerCollider.bounds.center - rb.position;
+            subVec.y = 0;
+
+            rb.linearVelocity = subVec.normalized * moveSpeed;
+
+            // プレイヤーの方向を向く
+            var rotateTarget = subVec.normalized;
+            Vector3 forward = transform.forward;
+            transform.forward = Vector3.Slerp(forward, rotateTarget,
+                rotateSpeed * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,38 +8,62 @@ public class Player : MonoBehaviour
     [SerializeField] float rotateSpeed;
     [SerializeField] Animator animator;
     [SerializeField] float jumpSpeed;
+    [SerializeField] float groundNormalYMin = 0.7f;
+    [SerializeField] float groundDamping = 8f;
+    [SerializeField] float airDamping = 0.5f;
 
     PlayerInput playerInput;
     Rigidbody rb;
     Vector3 rotateTarget;
+    bool isGrounded = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody>();
+        rb.sleepThreshold = -1;
+    }
+
+    private void FixedUpdate()
+    {
+        // 減衰を地上と空中で変える
+        if(isGrounded)
+        {
+            rb.linearDamping = groundDamping;
+        }
+        else
+        {
+            rb.linearDamping = airDamping;
+        }
+
+        // 物理計算中に接地判定を行うため、一旦ここで false にしておく
+        isGrounded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var accelVec = playerInput.actions["Move"].ReadValue<Vector2>();
-
-        var cameraDir = playerInput.camera.transform.forward;
-        cameraDir.y = 0;
-        cameraDir = cameraDir.normalized;
-
-        var cameraRight = playerInput.camera.transform.right;
-
-        var accelVec3D =
-            cameraDir * accelVec.y * accel
-            + cameraRight * accelVec.x * accel;
-        rb.AddForce(accelVec3D, ForceMode.Acceleration);
-
-        // プレイヤーの向きを変える
-        if(accelVec3D != Vector3.zero)
+        if (isGrounded)
         {
-            rotateTarget = accelVec3D.normalized;
+            var accelVec = playerInput.actions["Move"].ReadValue<Vector2>();
+
+            var cameraDir = playerInput.camera.transform.forward;
+            cameraDir.y = 0;
+            cameraDir = cameraDir.normalized;
+
+            var cameraRight = playerInput.camera.transform.right;
+
+            var accelVec3D =
+                cameraDir * accelVec.y * accel
+                + cameraRight * accelVec.x * accel;
+            rb.AddForce(accelVec3D, ForceMode.Acceleration);
+
+            // プレイヤーの向きを変える
+            if (accelVec3D != Vector3.zero)
+            {
+                rotateTarget = accelVec3D.normalized;
+            }
         }
 
         // 前方向をコピーしておく
@@ -49,8 +73,11 @@ public class Player : MonoBehaviour
         transform.up = Vector3.up;
 
         // 前方向をターゲットに向かって補間
-        transform.forward =
-            Vector3.Slerp(forward, rotateTarget, rotateSpeed * Time.deltaTime);
+        var targetForward = Vector3.Slerp(forward, rotateTarget, rotateSpeed * Time.deltaTime);
+        if (targetForward != Vector3.zero)
+        {
+            transform.forward = targetForward;
+        }
 
         // アニメーターのMoveSpeedパラメータに Rigidbody の移動速度の大きさを与える
         Vector3 velocityXZ = rb.linearVelocity;
@@ -58,10 +85,22 @@ public class Player : MonoBehaviour
         animator.SetFloat("MoveSpeed", velocityXZ.magnitude);
 
         // ジャンプ
-        if (playerInput.actions["Jump"].WasPressedThisFrame())
+        if (playerInput.actions["Jump"].WasPressedThisFrame()
+            && isGrounded)
         {
             Vector3 jumpVec = new Vector3(0, jumpSpeed, 0);
             rb.AddForce(jumpVec, ForceMode.VelocityChange);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        foreach (var contact in collision.contacts)
+        {
+            if(contact.normal.y >= groundNormalYMin)
+            {
+                isGrounded = true;
+            }
         }
     }
 }
